bdafb8bbc1ad087610879277f716a676cc4bdf6c
ChaitanyaBoggavarapu <chaitu.chowdary00@gmail.com>
2019-09-05 04:12:55 -0600

    Create numberofusers.py

---
+++ numberofusers.py
@@ -0,0 +1,15 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+driver = webdriver.Chrome()
+#driver.get("http://www.python.org")
+driver.get("http://admin:Chennaisuperkings7@routerlogin.net/")
+
+#a = driver.find_element_by_id("AttachedDevices-condition")
+
+driver.switch_to.frame("page")
+
+b = driver.page_source
+
+result = b.find('<span id="AttachedDevices-condition" class="Condition-normal">')
