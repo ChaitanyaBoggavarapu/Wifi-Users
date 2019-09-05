c90351268245620de344386f616cab4f388afadf
ChaitanyaBoggavarapu <chaitu.chowdary00@gmail.com>
2019-09-05 04:26:12 -0600

    Update numberofusers.py

--- numberofusers.py
+++ numberofusers.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.keys import Keys
 import time
 driver = webdriver.Chrome()
 #driver.get("http://www.python.org")
-driver.get("http://username:password@routerlogin.net/")
+driver.get("http://admin:Chennaisuperkings7@routerlogin.net/")
 
 #a = driver.find_element_by_id("AttachedDevices-condition")
 
@@ -14,4 +14,7 @@ b = driver.page_source
 
 result = b.find('<span id="AttachedDevices-condition" class="Condition-normal">')
 
+driver.implicitly_wait(15)
+
+driver.quit()
 #Result gives the postion of the span id
