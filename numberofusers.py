d521391f79f95b001e26afe15d6ad56897b1df90
ChaitanyaBoggavarapu <chaitu.chowdary00@gmail.com>
2019-09-05 04:14:06 -0600

    Update numberofusers.py

--- numberofusers.py
+++ numberofusers.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.keys import Keys
 import time
 driver = webdriver.Chrome()
 #driver.get("http://www.python.org")
-driver.get("http://admin:Chennaisuperkings7@routerlogin.net/")
+driver.get("http://username:password@routerlogin.net/")
 
 #a = driver.find_element_by_id("AttachedDevices-condition")
 
