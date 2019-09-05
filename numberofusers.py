29ef27a6979756f5c5a97852ee191c33d8d548d7
ChaitanyaBoggavarapu <chaitu.chowdary00@gmail.com>
2019-09-05 12:01:30 -0600

    Update numberofusers.py

--- numberofusers.py
+++ numberofusers.py
@@ -1,20 +1,24 @@
-
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
-driver = webdriver.Chrome()
-#driver.get("http://www.python.org")
-driver.get("http://admin:Chennaisuperkings7@routerlogin.net/")
-
-#a = driver.find_element_by_id("AttachedDevices-condition")
-
-driver.switch_to.frame("page")
-
-b = driver.page_source
-
-result = b.find('<span id="AttachedDevices-condition" class="Condition-normal">')
-
-driver.implicitly_wait(15)
-
-driver.quit()
-#Result gives the postion of the span id
+ numberofusers = 0
+    driver = webdriver.Chrome()
+    #driver.get("http://www.python.org")
+    driver.get("http://admin:Chennaisuperkings7@routerlogin.net/")
+    
+    #a = driver.find_element_by_id("AttachedDevices-condition")
+    
+    driver.switch_to.frame("page")
+    
+    b = driver.page_source
+    
+    result = b.find('<span id="AttachedDevices-condition" class="Condition-normal">')
+    print(b[result+62])
+    if (b[result+63]!='<'):
+        result1 = b[result+63]
+        numberofusers = str(result)+str(result1)
+    else:
+        numberofusers = str(result)
+    #print(b[result+63])
+    #print(b[result+64])
+    print(numberofusers)
+    driver.implicitly_wait(15)
+    
+    driver.quit()
